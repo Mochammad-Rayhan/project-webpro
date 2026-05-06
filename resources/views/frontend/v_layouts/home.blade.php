143b9fc1fd60dc15aab74815b665fdf8dc5f4aaf
Kamila zulfa <kamilazulfaa22@gmail.com>
2026-05-06 16:38:38 +0700

    ubah title home

--- resources/views/frontend/v_layouts/home.blade.php
+++ resources/views/frontend/v_layouts/home.blade.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Str;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Beautycare</title>
+    <title>Beauty care</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
